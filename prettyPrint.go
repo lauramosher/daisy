1c9776492cb5cf79ea22a8ae847afcf65f107bb4
Laura Mosher <laura.trevisan@gmail.com>
2018-03-29 10:31:12 -0400

    Update prettyPrint and add colors

--- prettyPrint.go
+++ prettyPrint.go
@@ -5,9 +5,12 @@ import (
 )
 
 const(
+  blue = "\033[0;34m"
+  green = "\033[0;32m"
   red = "\033[0;31m"
   purple = "\033[0;35m"
   nc = "\033[0m"
+  yellow = "\033[0;33m"
 )
 
 
@@ -15,8 +18,20 @@ func printPlain(text string) {
   fmt.Println(text)
 }
 
-func printInfo(text string) {
+func printCallout(text string) {
   fmt.Printf(purple)
+  fmt.Println(text)
+  fmt.Printf(nc)
+}
+
+func printInfo(text string) {
+  fmt.Printf(blue)
+  fmt.Println(text)
+  fmt.Printf(nc)
+}
+
+func printWarn(text string) {
+  fmt.Printf(yellow)
   fmt.Println(text)
   fmt.Printf(nc)
 }
