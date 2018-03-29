d330f706fd6a8071c221c08714bab312f03e26b5
Laura Mosher <laura.trevisan@gmail.com>
2018-03-29 16:30:35 -0400

    Remove --back flag in favor of the global return command

--- break.go
+++ break.go
@@ -1,26 +1,10 @@
 package main
 
 import (
-  "os"
   "time"
 )
 
 func setBreak(args []string) {
-  if len(args) < 1 {
-    setAway()
-    os.Exit(1)
-  }
-  switch {
-  case args[0] == "-b":
-    setBack()
-  case args[0] == "--back":
-    setBack()
-  default:
-    setAway()
-  }
-}
-
-func setAway() {
   setPresence("away")
 
   if time.Now().Hour() > 12 {
@@ -28,9 +12,4 @@ func setAway() {
   } else {
     setStatus("Elevensies", ":croissant:")
   }
-}
-
-func setBack() {
-  setPresence("auto")
-  setStatus("Working Remotely", ":house_with_garden:")
 }
