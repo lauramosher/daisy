ebfda0517f908574105e90b85eca83e7bc62c0a9
Laura Mosher <laura.trevisan@gmail.com>
2018-04-05 15:38:07 -0400

    Update presence messaging

--- cmd/day/presence.go
+++ cmd/day/presence.go
@@ -6,29 +6,44 @@ import (
 )
 
 func back(args []string) {
+  util.PrintPlain("Setting Slack presence...\t\t")
   slack.SetPresence("auto")
+  util.PrintClear("Done!\n")
+
+  util.PrintPlain("Setting Slack status...\t\t\t")
   slack.SetStatus("Working remotely", ":house_with_garden:")
+  util.PrintClear("Done!\n")
 
   if util.Include(args, "-m") || util.Include(args, "--message") {
     for i, v := range args {
       if v == "-m" || v =="--message" {
+        util.PrintPlain("Posting message to Slack...\t\t")
         value :=  args[i+1]
         slack.PostMessage(value)
+        util.PrintClear("Done!\n")
       }
       break
     }
   }
+  util.PrintCallout("Welcome back!\n")
 }
 
 func away(args []string) {
+  util.PrintPlain("Setting Slack presence...\t\t")
   slack.SetPresence("away")
+  util.PrintClear("Done!\n")
+
+  util.PrintPlain("Setting Slack status...\t\t\t")
   slack.SetStatus("Heads down", ":headphones:")
+  util.PrintClear("Done!\n")
 
   if util.Include(args, "-m") || util.Include(args, "--message") {
     for i, v := range args {
       if v == "-m" || v =="--message" {
+        util.PrintPlain("Posting message to Slack...\t\t")
         value :=  args[i+1]
         slack.PostMessage(value)
+        util.PrintClear("Done!\n")
       }
       break
     }
