34548510b228f50d1103c03488fc206128ef390c
Laura Mosher <laura.trevisan@gmail.com>
2018-04-05 14:56:47 -0400

    Update day break messaging

--- cmd/day/break.go
+++ cmd/day/break.go
@@ -8,9 +8,17 @@ import (
 )
 
 func setBreak(args []string) {
+  util.PrintPlain("Setting Slack status & Presence...\t\t")
   slack.SetPresence("away")
+  if time.Now().Hour() > 12 {
+    slack.SetStatus("Afternoon Tea", ":tea:")
+  } else {
+    slack.SetStatus("Elevensies", ":croissant:")
+  }
+  util.PrintClear("Done!\n")
 
   if util.Include(args, "-m") || util.Include(args, "--message") {
+    util.PrintPlain("Posting message to Slack...\t\t\t")
     for i, v := range args {
       if v == "-m" || v =="--message" {
         value :=  args[i+1]
@@ -18,11 +26,8 @@ func setBreak(args []string) {
       }
       break
     }
+    util.PrintClear("Done!\n")
   }
 
-  if time.Now().Hour() > 12 {
-    slack.SetStatus("Afternoon Tea", ":tea:")
-  } else {
-    slack.SetStatus("Elevensies", ":croissant:")
-  }
+  util.PrintCallout("Enjoy your break!\n")
 }
